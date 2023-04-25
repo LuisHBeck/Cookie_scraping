abec30cd9f37e9f9d0a44b7d872f1692adb0a643
Luís Beck <125098484+LuisHBeck@users.noreply.github.com>
2023-04-25 11:19:57 -0300

    Add files via upload

--- web02.py
+++ web02.py
@@ -21,15 +21,30 @@ class Web():
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
 
+    # def abrir_site(self):
+    #     sleep(2)
+    #     self.driver.get(self.site)
+    #     sleep(2)
+    #     k = 0
+    #     for i in range(110):
+    #         print()
+    #         print(self.driver.find_element(By.XPATH, self.map['sorteio']['xpath'].replace("%num%", f'{i+4}')).text)
+
+    #         for j in range(6):
+    #             print(self.driver.find_element(By.XPATH, self.map['numero']['xpath'].replace("%num%", f'{k+1}')).text, end="  ")
+    #             k+=1
+    #         print()
+               
+    #     sleep(2)
+
     def abrir_site(self):
         sleep(2)
         self.driver.get(self.site)
         sleep(2)
         k = 0
         for i in range(110):
-            print()
-            print(self.driver.find_element(By.XPATH, self.map['sorteio']['xpath'].replace("%num%", f'{i+4}')).text)
-
+            sorteio = self.driver.find_element(By.XPATH, self.map['sorteio']['xpath'].replace("%num%", f'{i+4}')).text
+            
             for j in range(6):
                 print(self.driver.find_element(By.XPATH, self.map['numero']['xpath'].replace("%num%", f'{k+1}')).text, end="  ")
                 k+=1
